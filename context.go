33049912205ba611ce3952f89e337675b95d6eb7
Quinn Slack <qslack@qslack.com>
2013-09-06 16:59:59 -0700

    Fix docs (returns nil, not 0)

--- context.go
+++ context.go
@@ -11,7 +11,7 @@ const (
 	viewID contextKey = iota
 )
 
-// GetViewID returns the view ID stored in the HTTP request context, or 0 if none
+// GetViewID returns the view ID stored in the HTTP request context, or nil if none
 // exists.
 func GetViewID(r *http.Request) (id *ViewID) {
 	v := context.Get(r, viewID)
