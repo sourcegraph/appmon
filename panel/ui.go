bc9f70c22b0495818a771b32302cbadafaecd64a
Quinn Slack <qslack@qslack.com>
2013-10-24 06:10:45 -0700

    Add views panel

--- panel/ui.go
+++ panel/ui.go
@@ -16,6 +16,7 @@ import (
 const (
 	trackUICalls = "track:ui:calls"
 	trackUIMain  = "track:ui:main"
+	trackUIViews = "track:ui:views"
 )
 
 var baseHref string
@@ -24,10 +25,159 @@ var baseHref string
 func UIRouter(theBaseHref string, rt *mux.Router) *mux.Router {
 	baseHref = theBaseHref
 	rt.Path("/calls").Methods("GET").HandlerFunc(uiCalls).Name(trackUICalls)
+	rt.Path("/views").Methods("GET").HandlerFunc(uiViews).Name(trackUIViews)
 	rt.Path("/").Methods("GET").HandlerFunc(uiMain).Name(trackUIMain)
 
 	return rt
 }
+
+func uiViews(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+
+	lastNHoursStr := q.Get("lastNHours")
+	var lastNHours int
+	if lastNHoursStr == "" {
+		lastNHours = 7
+	} else {
+		var err error
+		lastNHours, err = strconv.Atoi(lastNHoursStr)
+		if err != nil {
+			http.Error(w, "bad 'lastNHours' parameter: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+	}
+
+	sort := q.Get("sort")
+	if sort == "" {
+		sort = "date"
+	}
+	okSorts := map[string]struct{}{"date": struct{}{}}
+	if _, ok := okSorts[sort]; !ok {
+		http.Error(w, "bad 'sort' parameter", http.StatusBadRequest)
+		return
+	}
+
+	viewStates, err := getViewStates(lastNHours)
+	if err != nil {
+		http.Error(w, "getViewStates failed: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var views []*track.View
+	selectedState := q.Get("state")
+	if selectedState != "" {
+		views, err = track.QueryViews(`WHERE state = $1 AND (current_timestamp - date < ($2::int * interval '1 hour')) ORDER BY `+sort+` DESC LIMIT 100`, selectedState, lastNHours)
+		if err != nil {
+			http.Error(w, "QueryViews failed: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+
+	tmpl(trackUIViews, uiViewsHTML)(w, struct {
+		common
+		LastNHours    int
+		Sort          string
+		ViewStates    []*viewState
+		SelectedState string
+		Views         []*track.View
+	}{
+		common:        newCommon("Views"),
+		LastNHours:    lastNHours,
+		Sort:          sort,
+		ViewStates:    viewStates,
+		SelectedState: selectedState,
+		Views:         views,
+	})
+}
+
+type viewState struct {
+	State string
+	Count int
+}
+
+func getViewStates(lastNHours int) (viewStates []*viewState, err error) {
+	var rows *sql.Rows
+	viewStateSQL := `
+      SELECT * FROM (
+        SELECT v.state, COUNT(v.*) AS count
+        FROM track.view v
+        WHERE current_timestamp - date < ($1::int * interval '1 hour')
+        GROUP BY state
+      ) q ORDER BY count DESC
+`
+	rows, err = track.DB.Query(viewStateSQL, lastNHours)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		vs := new(viewState)
+		err = rows.Scan(&vs.State, &vs.Count)
+		if err != nil {
+			return
+		}
+		viewStates = append(viewStates, vs)
+	}
+	return
+}
+
+var uiViewsHTML = `
+<h1>Views</h1>
+<div class="row">
+  <div class="col-md-2">
+    <form action="views" method="get" class="form">
+      {{if .SelectedState}}<input type="hidden" name="state" value="{{.SelectedState}}">{{end}}
+      <div class="form-group">
+        <label for="lastNHours">Last # hours</label>
+        <input type="number" class="form-control" id="lastNHours" name="lastNHours" placeholder="#" value="{{.LastNHours}}">
+      </div>
+      <div class="form-group">
+        <label>Sort order:</label>
+        <div class="radio">
+          <label><input type="radio" name="sort" value="date" {{if eq .Sort "date"}}checked{{end}}> Most recent</label>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Update list</button>
+    </form>
+  </div>
+  <div class="col-md-3">
+    <div class="list-group">
+    {{$LastNHours := .LastNHours}}
+    {{$Sort := .Sort}}
+    {{$SelectedState := .SelectedState}}
+    {{range .ViewStates}}
+      <a href="views?sort={{$Sort}}&lastNHours={{$LastNHours}}&state={{.State}}" class="list-group-item {{if eq $SelectedState .State}}active{{end}}">
+        {{.State}}
+        <span class="badge">{{.Count}}</span>
+      </a>
+    {{else}}
+      <li><div class="alert alert-error">No states to show.</div></li>
+    {{end}}
+    </div>
+  </div>
+  <div class="col-md-7">
+     {{if eq .SelectedState ""}}
+       <div class="alert alert-warning">Select a state.</div>
+     {{else}}
+       <table class="table">
+         <thead><tr><th>Date</th><th>URL</th>
+         <tbody>
+           {{range .Views}}
+             <tr>
+               <td>
+                 {{.Date.Format "2006-01-02 15:04:05"}}<br>
+                 <span class="text-muted">{{timeAgo .Date}}</span>
+               </td>
+               <td style="word-wrap:break-word;max-width:200px;"><a href="{{.RequestURI}}" target="_blank">{{.RequestURI}}</a></td>
+             </tr>
+           {{else}}
+             <tr><td colspan="5" class="alert alert-warning">No views found for state {{$SelectedState}}.</td></tr>
+           {{end}}
+         </tbody>
+       </table>
+     {{end}}
+  </div>
+</div>
+`
 
 func uiCalls(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
